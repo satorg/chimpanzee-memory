07bd84fc911dd7a97554e5d91219c394741c6235
satorg <satorg@gmail.com>
2016-09-07 22:07:17 +0300

    initial app version

--- src/main/scala/satorg/chimp/App.scala
+++ src/main/scala/satorg/chimp/App.scala
@@ -1,9 +1,88 @@
 package satorg.chimp
 
+import org.scalajs.dom
+
 import scala.scalajs.js.JSApp
 
 object App extends JSApp {
+  private val canvas = dom.document.getElementById("canvas").asInstanceOf[dom.html.Canvas]
+  private val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+  private val (canvasWidth, canvasHeight) = (canvas.width, canvas.height)
+
+  private final val minCellCount = 5
+
+  private final val cellBorder = 2
+  private final val cellBorderDual = cellBorder * 2
+
+  private val cellSize = {
+    val maxCellWidth = (canvasWidth / minCellCount) >> 1 << 1
+    val maxCellHeight = (canvasHeight / minCellCount) >> 1 << 1
+    math.min(maxCellWidth, maxCellHeight)
+  }
+  private val cellSizeHalf = cellSize / 2
+  private val cellInnerSize = cellSize - cellBorderDual
+
+  private val colCount = canvasWidth / cellSize
+  private val rowCount = canvasHeight / cellSize
+
+  private def newState() = State(colCount, rowCount)
+
+  var state = newState()
+
   override def main(): Unit = {
-    
+    canvas.onclick = onClickCanvas _
+
+    ctx.font = s"bold ${cellSize}px sans-serif"
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+
+    drawState()
+  }
+
+  private def drawState(): Unit = {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    ctx.fillStyle = if (state.isFailed) "Red" else "White"
+
+    if (state.nextNumber == 1 || state.isFailed)
+      for (((col, row), index) <- state.numberPositions.zipWithIndex) {
+        val numStr = (index + state.nextNumber).toString
+        val numX = col * cellSize + cellSizeHalf
+        val numY = row * cellSize + cellSizeHalf
+
+        ctx.fillText(numStr, numX, numY)
+      }
+    else
+      for ((col, row) <- state.numberPositions) {
+        ctx.fillRect(
+          col * cellSize + cellBorder,
+          row * cellSize + cellBorder,
+          cellInnerSize,
+          cellInnerSize)
+      }
+  }
+
+  private def onClickCanvas(ev: dom.MouseEvent): Unit = {
+    if (state.isFinished) {
+      state = newState()
+      drawState()
+      return
+    }
+
+    val clickedPos = {
+      val rect = canvas.getBoundingClientRect()
+      val evX = (ev.clientX - rect.left).toInt
+      val evY = (ev.clientY - rect.top).toInt
+      (evX / cellSize, evY / cellSize)
+    }
+
+    if (state.numberPositions.head == clickedPos) {
+      state = state.dropNumber()
+      drawState()
+    }
+    else if (state.numberPositions.tail.contains(clickedPos)) {
+      state = state.fail()
+      drawState()
+    }
   }
 }
---
+++ src/main/scala/satorg/chimp/State.scala
@@ -0,0 +1,43 @@
+package satorg.chimp
+
+import scala.collection.mutable
+import scala.util.Random
+
+class State(val numberPositions: List[(Int, Int)], val nextNumber: Int = 1, val isFailed: Boolean = false) {
+
+  def isFinished = numberPositions.isEmpty || isFailed
+
+  def fail(): State = copy(isFailed = true)
+
+  def dropNumber(): State = {
+    if (isFailed) this
+    else
+      copy(
+        numberPositions = numberPositions.tail,
+        nextNumber = nextNumber + 1)
+  }
+
+  private def copy(numberPositions: List[(Int, Int)] = this.numberPositions,
+                   nextNumber: Int = this.nextNumber,
+                   isFailed: Boolean = this.isFailed) =
+    new State(numberPositions, nextNumber, isFailed)
+}
+
+object State {
+  private val random = new Random
+
+  private def generateNumberPositions(colCount: Int, rowCount: Int): List[(Int, Int)] = {
+    val numbers = mutable.ListBuffer.empty[(Int, Int)]
+    for (order <- 1 to 9) {
+      numbers +=
+        Stream.
+          continually {(random.nextInt(colCount), random.nextInt(rowCount))}.
+          dropWhile {numbers.contains}.
+          head
+    }
+    numbers.result()
+  }
+
+  def apply(colCount: Int, rowCount: Int): State =
+    new State(generateNumberPositions(colCount, rowCount), nextNumber = 1, isFailed = false)
+}
